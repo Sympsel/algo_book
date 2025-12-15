ff6b8cc87dc7c8aca322d9d7f5d52cf9346cd676
sympsel <sympsel@outlook.com>
2025-12-15 16:38:06 +0800

    day25_12_15 二分

--- 1.7.6_二分答案_跳石头.cpp
+++ 1.7.6_二分答案_跳石头.cpp
@@ -1,14 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const int N = 5 * (int)1e4 + 5;
 
+int a[N];
+int L, n, m;
 
+ll calc(int d) {
+	ll cnt{};
+	int i{};
+	while (i < n + 1) {
+		int j{i + 1};
+		while (j < n + 2 && a[j] - a[i] < d) {
+			++j;
+		}
+		cnt += j - i - 1;
+		i = j;
+	}
+	return cnt;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(nullptr);
 	cin.tie(nullptr);
-
 	
+	cin >> L >> n >> m;
+	a[0] = 0;
+	for (int i{1}; i <= n; ++i) cin >> a[i];
+	a[n + 1] = L;
+	
+	int l{1}, r{L};
+	while (l < r) {
+		int mid{l + (r - l) / 2 + 1};
+		if (calc(mid) <= m) {
+			l = mid;
+		} else {
+			r = mid - 1;
+		}
+	}
+	
+	cout << l << "\n";
 
 	return 0;
 }
