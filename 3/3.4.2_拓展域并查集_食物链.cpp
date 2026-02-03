f3ca15dec9b8c2e7dfc553df7dacbd762a534e01
sympsel <sympsel@outlook.com>
2026-02-03 22:28:42 +0800

    day26_2_3 拓展域并查集

--- 3/3.4.2_拓展域并查集_食物链.cpp
+++ 3/3.4.2_拓展域并查集_食物链.cpp
@@ -1,14 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 5e4 + 7;
+int n, k;
+char op;
+int uf[N * 3];
 
+int fd(int x) {
+	return uf[x] == x ? x : uf[x] = fd(uf[x]);
+}
+
+void un(int x, int y) {
+	if (x != y) uf[fd(y)] = fd(x);
+}
+
+bool issame(int x, int y) {
+	return x == y ? true : fd(x) == fd(y);
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(nullptr);
 	cin.tie(nullptr);
 
-	
+	cin >> n >> k;
+	for (int i{1}; i <= n * 3; ++i) uf[i] = i;
+	int ret{};
+	int x, y;
+	while (k--) {
+		cin >> op >> x >> y;
+		if (x > n || y > n) {
+			++ret;
+			continue;
+		}
+		if (op == '1') {
+			if (issame(x, y + n) || issame(y, x + n)) {
+				++ret;
+				continue;
+			}
+			un(x, y);
+			un(x + n, y + n);
+			un(x + 2 * n, y + 2 * n);
+		} else if (op == '2') {
+			if (issame(x, y) || issame(y, x + n)) {
+				++ret;
+				continue;
+			}
+			
+			un(x, y + n);
+			un(y, x + 2 * n);
+			un(x + n, y + 2 * n);
+			
+		} else {}
+	}	
+	cout << ret << "\n";
 
 	return 0;
 }
